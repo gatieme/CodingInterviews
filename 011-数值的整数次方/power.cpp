150dbec102884bb35cd0e57085ed2a1319c828ae
gatieme <gatieme@163.com>
2016-04-11 15:59:40 +0800

    ...

---
+++ 011-数值的整数次方/power.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+//  µ÷ÊÔ¿ª¹Ø
+#define __tmain main
+
+#ifdef __tmain
+
+#define debug cout
+
+#else
+
+#define debug 0 && cout
+
+#endif // __tmain
+
+
+
+class Solution
+{
+public:
+    double Power(double base, int exponent)
+    {
+
+
+    }
+};
+
+
+
+int __tmain( )
+{
+    debug <<"test" <<endl;
+    return 0;
+}
