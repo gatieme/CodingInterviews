a0eadc195591a9ad891c8898abfa0115ffb98ed2
gatieme <gatieme@163.com>
2016-04-12 12:46:55 +0800

    14...

--- 014-调整数组顺序使奇数位于偶数前面/reOrderArray.cpp
+++ 014-调整数组顺序使奇数位于偶数前面/reOrderArray.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -16,10 +17,49 @@ using namespace std;
 #endif // __tmain
 
 
+class Solution
+{
+public:
+    void reOrderArray(vector<int> &array)
+    {
+        vector<int>::iterator left = array.begin( );
+        vector<int>::iterator right = array.end( );
+
+        right --;
+        while(left != right)
+        {
+            debug <<"left = " <<*left <<", right = " <<*right <<endl;
+            system("PAUSE");
+
+            if(((*left & 1) == 0)
+              && ((*right & 1) == 1))
+            {
+                swap(left, right);
+            }
+            left++;
+            right--;
+        }
+
+    }
+};
 
 
 int __tmain( )
 {
-    debug <<"test" <<endl;
+    int array[] = {2, 4, 6, 1, 3, 5};
+    vector<int> vec(array, array + 6);
+    for(int i = 0; i < 6; i++)
+    {
+        cout <<vec[i] <<" ";
+    }
+    cout <<endl;
+    Solution solu;
+    solu.reOrderArray(vec);
+
+    for(int i = 0; i < 6; i++)
+    {
+        cout <<vec[i] <<" ";
+    }
+    cout <<endl;
     return 0;
 }
