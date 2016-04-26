a6048cddfd3e1aa006a7516881be4b1d35f024fc
gatieme <gatieme@163.com>
2016-04-26 16:39:26 +0800

    。。。

---
+++ 030-最小的K个数/leastknum.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+//  µ÷ÊÔ¿ª¹Ø
+#define __tmain main
+
+#ifdef __tmain
+
+#define debug cout
+
+#else
+
+#define debug 0 && cout
+
+#endif // __tmain
+
+class Solution {
+public:
+    vector<int> GetLeastNumbers_Solution(vector<int> input, int k) {
+        
+    }
+};
+
+
+int __tmain( )
+{
+    debug <<"test" <<endl;
+    return 0;
+}
