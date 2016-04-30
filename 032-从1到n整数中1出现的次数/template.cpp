cf44bd5fc96e0259d8bb2e001f6c8ec5c1502040
gatieme <gatieme@163.com>
2016-04-30 21:41:50 +0800

    ...

---
+++ 032-从1到n整数中1出现的次数/template.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+//  µ÷ÊÔ¿ª¹Ø
+#define __tmain main
+
+#ifdef __tmain
+
+#define debug cout
+
+#else
+
+#define debug 0 && cout
+
+#endif // __tmain
+
+class Solution
+{
+public:
+    int NumberOf1Between1AndN_Solution(int n)
+    {
+    
+    }
+};
+
+
+int __tmain( )
+{
+    debug <<"test" <<endl;
+    return 0;
+}
