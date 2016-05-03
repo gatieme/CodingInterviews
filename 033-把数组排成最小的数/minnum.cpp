8d7cd6b859e0003499827dce3d2fd50734528fe4
gatieme <gatieme@163.com>
2016-05-03 13:18:22 +0800

    string to int...

--- 033-把数组排成最小的数/minnum.cpp
+++ 033-把数组排成最小的数/minnum.cpp
@@ -30,39 +30,42 @@ string ToString(const T& t)
     return oss.str( );  //  获取转换后的字符转并将其写入result
 }
 
-class Compare
-{
 
-public:
-
-bool operator( )(string &left, string &right)
-{
-    string leftright = left + right;
-    string rightleft = right + left;
-    if(leftright > rightleft)
-    {
-        debug <<leftright <<" > " <<rightleft <<" -- " <<rightleft <<endl;
-    }
-    else
-    {
-        debug <<leftright <<" < " <<rightleft <<" -- " <<leftright <<endl;
-    }
-
-    return leftright < rightleft;
-}
-
-};
 class Solution
 {
 public:
+
+    ///  比较函数
+    //  我们比较的不是两个字符串本身的大小，而是他们拼接后的两个数字的大小
+    //  在比较时，采用特别的思路----把两个字符串进行拼接，
+    //  如果字符串1排在前面的数小，那么就把字符串1放到前面。
+
+
+    static bool Compare(const string &left, const string &right)
+    {
+        string leftright = left + right;
+        string rightleft = right + left;
+        if(leftright > rightleft)
+        {
+            debug <<leftright <<" > " <<rightleft <<" -- " <<rightleft <<endl;
+        }
+        else
+        {
+            debug <<leftright <<" < " <<rightleft <<" -- " <<leftright <<endl;
+        }
+
+        return leftright < rightleft;
+    }
 
 
     string PrintMinNumber(vector<int> numbers)
     {
         string res = "";
+        string str;
 
         vector<string> strNum;
-        string str;
+
+        ///  将整数转换成字符串
         for(unsigned int i = 0; i < numbers.size( ); i++)
         {
             str = ToString(numbers[i]);
@@ -70,8 +73,10 @@ public:
             debug <<str <<endl;
         }
 
-        sort(strNum.begin( ), strNum.end( ), Compare());
+        ///  对字符串按照拼接后的大小进行排序
+        sort(strNum.begin( ), strNum.end( ), Compare);
 
+        ///  拼接结果
         for(unsigned int i = 0; i < strNum.size( ); i++)
         {
             debug <<strNum[i];
