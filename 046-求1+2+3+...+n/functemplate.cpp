00b2f43a512496fe0bf471760da7ce58f486babe
gatieme <gatieme@163.com>
2016-05-23 23:34:51 +0800

    剑指Offer--046-求1+2+3+...+n--http://blog.csdn.net/gatieme/article/details/51485713

---
+++ 046-求1+2+3+...+n/functemplate.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using namespace std;
+
+//  调试开关
+#define __tmain main
+
+#ifdef __tmain
+
+#define debug cout
+
+#else
+
+#define debug 0 && cout
+
+#endif // __tmain
+
+
+template < unsigned int n>
+struct Sum
+{
+    enum Value
+    {
+        N = Sum< n - 1 >::N + n
+    };
+};
+
+template <>
+struct Sum<1>
+{
+    enum Value
+    {
+        N = 1
+    };
+};
+
+
+int main( )
+{
+
+    cout <<Sum<100>::N <<endl;
+
+    return 0;
+}
+
+
+
---
+++ 046-求1+2+3+...+n/macro.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+//  调试开关
+#define __tmain main
+
+#ifdef __tmain
+
+#define debug cout
+
+#else
+
+#define debug 0 && cout
+
+#endif // __tmain
+
+
+class base;
+base *parray[2];
+
+class base{
+public:
+    virtual unsigned int sum(unsigned int n)
+    {
+        //  递归的终止
+        return 0;
+    }
+};
+
+class derive :public base{
+public:
+    virtual unsigned int sum(unsigned int n)
+    {
+        //  !!n两次非运算, 从而将n转换为bool类型, 来选择函数
+        //  sum(n) = sum(n - 1) + n 则是递归公式
+        return parray[!!n]->sum(n - 1) + n;
+    }
+};
+
+
+class Solution {
+public:
+    int Sum_Solution(int n) {
+
+        base a;
+        derive b;
+
+        parray[0] = &a;
+        parray[1] = &b;
+
+        return parray[1]->sum(n);
+    }
+};
+
+
+int main(){
+
+    Solution s;
+
+    cout<<s.Sum_Solution(10);
+
+    return 0;
+}
+
+
+
