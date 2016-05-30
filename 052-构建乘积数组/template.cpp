e6c3a88162cab99d0980a93a04e7137b5a7ac2f3
gatieme <gatieme@163.com>
2016-05-30 21:04:34 +0800

    ...

--- 052-构建乘积数组/template.cpp
+++ 052-构建乘积数组/template.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -17,7 +18,30 @@ using namespace std;
 #endif // __tmain
 
 
+class Solution
+{
+public:
+    vector<int> multiply(const vector<int>& A)
+    {
+        int n = A.size();
+        vector<int> res(n);
+        // C[i] = A[0] * A[1] * ... *A[i - 1]
+        for(int i = 0, temp = 1; i < n; i++)
+        {
+            res[i] = temp;
+            temp *= A[i];
+        }
 
+        // D[i] = A[i + 1] * A[i + 2] * ... *A[n - 1]
+        for(int i = n - 1, temp = 1; i >= 0; i--)
+        {
+            res[i] *= temp;
+            temp *= A[i];
+
+        }
+        return res;
+    }
+};
 
 int __tmain( )
 {
