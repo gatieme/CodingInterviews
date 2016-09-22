82641aa505c99983339a7fb64fc57df6d30bbaef
aheadlead <aheadlead@dlifep.com>
2016-09-22 16:29:35 +0800

    添加指针数组求和公式的方法

---
+++ 046-求1+2+3+...+n/pointerofarray.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int rich(int n) {
+    return ( (int)( &((uint8_t (*) [n])0)[1+n][0]) ) >> 1;
+}
+
+int main() {
+    printf("%d\n", rich(10));
+
+    return 0;
+}
+
